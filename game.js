e6bd85d8b01b2cd6fb0155433d63ba51598c1e30
Ayisha Alli <ayisha.alli@playtech.com>
2022-03-31 19:45:27 +0100

    refractured code that reruns page to makeit easier to read

--- game.js
+++ game.js
@@ -10,46 +10,30 @@ import { checkCollisions } from "./checkCollisions.js";
 export const gameBoard = document.getElementById("gameBoard");
 export const WIDTH = 21; // defined in CCS - grid width
 export const HEIGHT = 21; //// defined in CCS - grid height
+let running = true;
+let speed = 500; // The hieght the number, the faster the snake will move
 
-let speed = 6; // The hieght the number, the faster the snake will move 
-
-// Game Levels 
-const easy = document.getElementById('easy')
-const medium = document.getElementById('medium')
-const hard = document.getElementById('hard')
-
-easy.addEventListener('click', ()=>{
-  speed = 4
-  reload = location.reload();
-
-})
-medium.addEventListener('click', ()=>{
-  reload = location.reload();
-  speed = 8
-})
-hard.addEventListener('click', ()=>{
-  reload = location.reload();
-  speed = 16
-})
+// Game Levels
+// const easy = document.getElementById("easy");
+// const medium = document.getElementById("medium");
+// const hard = document.getElementById("hard");
 
 
 
-let lastRenderTime = 0;
-function main(currentTime) { // this function makes the page rerender so the icons can appear to be moving 
-  window.requestAnimationFrame(main);
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // lines 6&7 refresh/rereneder the page so the game can be updated
-  if (secondsSinceLastRender < 1 / speed) {
-    return;
+
+function main() {
+  if (running) {
+    setTimeout(function onTick() {
+      
+      update();
+      draw();
+
+      // Repeat
+      main();
+    }, 500);
   }
-  lastRenderTime = currentTime;
-
-  //update()
-  draw();
-
-  checkApples(snakeBody);
 }
-
-window.requestAnimationFrame(main);
+main();
 
 function update() {
   updateSnake();
@@ -63,8 +47,8 @@ function draw() {
 }
 
 export function gameOver() {
+  running = false;
   gameBoard.style.display = "none";
-  easy.classList.add('gameOverLevels')
-  medium.classList.add('gameOverLevels')
-  hard.classList.add('gameOverLevels')
+  //gameBoard.style.visibility = "hidden"
+  
 }
